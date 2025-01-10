923569fdd0753dc85aa0b2a525766e5a651e77f5
Melvin-KJ <melvinofficial2001@gmail.com>
2025-01-10 16:41:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import View from './pages/View';
+import Wishlist from './pages/Wishlist';
+import Cart from './pages/Cart';
+import Pnf from './pages/Pnf';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={Home} />
+        <Route path="/:id/view" element={View} />
+        <Route path='/wishlist' element={Wishlist}/>
+        <Route path='/cart' element={Cart}/>
+        <Route path='/' element={Pnf}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
